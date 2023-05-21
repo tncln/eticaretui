7b99a1bb63d01879ec1238c729da1e22abeebf40
Adem Tunçalın <31670075+tncln@users.noreply.github.com>
2023-05-21 19:01:26 +0300

    alertify özelleştirme

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType } from 'src/app/services/admin/alertify.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  constructor(){
 
+  }
+  ngOnInit(): void {
+    
+  }
 }
