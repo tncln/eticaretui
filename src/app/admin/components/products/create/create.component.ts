147165d1167efc14d9880201aaf52b21f6c83b8b
Adem Tunçalın <31670075+tncln@users.noreply.github.com>
2023-06-08 08:55:49 +0300

    error response fluent validation result handle

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { error } from 'jquery';
 import { NgxSpinner, NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { Create_Product } from 'src/app/contracts/create_product';
@@ -22,7 +23,7 @@ export class CreateComponent extends BaseComponent implements OnInit {
     const create_product: Create_Product = new Create_Product();
     create_product.name = name.value;
     create_product.stock = parseInt(stock.value);
-    create_product.price = parseFloat(price.value);
+    create_product.price = parseFloat(price.value); 
 
     this.productService.create(create_product,()=> {
       this.hideSpinner(SpinnerType.BallAtom);
@@ -31,6 +32,12 @@ export class CreateComponent extends BaseComponent implements OnInit {
         messageType : MessageType.Success,
         position : Position.TopRight
       });
+    }, errorMessage => {
+      this.alertify.message(errorMessage.toString(),{
+        dismissOthers:true,
+        messageType : MessageType.Error,
+        position : Position.TopRight
+      })
     });
   }
 }
